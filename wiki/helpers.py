c52f38334ba9b41275b870fcdbd5aa083887eccb
Eugene Van den Bulke <eugene.vandenbulke@gmail.com>
2012-05-08 17:10:41 +1000

    basic tests for normalize title

--- wiki/helpers.py
+++ wiki/helpers.py
@@ -5,6 +5,17 @@ def normalize_title(title):
 
     >>> normalize_title('human_body')
     'human body'
+    >>> normalize_title('humanBody')
+    'human body'
+    >>> normalize_title('Parts_of_the_HumanBody')
+    'parts of the human body'
+    >>> normalize_title('HumanBody/Parts')
+    'human body / parts'
+    >>> normalize_title('HumanBody /Parts')
+    'human body / parts'
     """
-
-    return title
+    title = re.sub(r'[_-]', ' ', title)
+    camel = re.compile('([A-Z][A-Z][a-z])|([a-z][A-Z])')
+    title = camel.sub(lambda m: m.group()[:1] + " " + m.group()[1:], title)
+    title = ' / '.join(map(lambda s: s.strip(), title.split('/')))
+    return title.lower()
--- wiki/tests.py
+++ wiki/tests.py
@@ -10,12 +10,5 @@ from django.test import TestCase
 
 import helpers
 
-#class SimpleTest(TestCase):
-    #def test_basic_addition(self):
-        #"""
-        #Tests that 1 + 1 always equals 2.
-        #"""
-        #self.assertEqual(1 + 1, 2)
-
 def suite():
     return doctest.DocTestSuite(helpers)
